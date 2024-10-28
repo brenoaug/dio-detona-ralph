ff8f04dc3b6204650584efe1a0779339d8c499c6
brenoaug <breaugustocp@outlook.com>
2024-10-28 11:50:07 -0300

    contador de tempo e responsividade

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -10,8 +10,30 @@ const state = {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
+    currentTime: 60,
   },
+  actions:{
+    timerID: setInterval(randomSquare, 1000),
+    countDownTimerID: setInterval(countDown, 1000),
+  }
 };
+
+function countDown(){
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  
+  if(state.values.currentTime <= 0){
+    clearInterval(state.actions.countDownTimerID)
+    clearInterval(state.actions.timerID);
+    alert('GAME OVER! SEU RESULTADO FINAL FOI: ' + state.values.result);
+  }
+}
+
+function playSound(audioName){
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.1;
+  audio.play();
+}
 
 function randomSquare(){
   state.view.square.forEach((square) =>{
@@ -24,24 +46,21 @@ function randomSquare(){
   state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy(){
-  state.values.timerID = setInterval(randomSquare, state.values.gameVelocity);
-}
 
 function addListenerHitBox(){
   state.view.square.forEach((square) => {
-    square.addEventListener("mousedown",() => {
+    square.addEventListener('mousedown',() => {
       if(square.id === state.values.hitPosition){
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
+        playSound('hit');
       }
     });
   });
 }
 
 function initialize(){
-  moveEnemy();
   addListenerHitBox();
 }
 
